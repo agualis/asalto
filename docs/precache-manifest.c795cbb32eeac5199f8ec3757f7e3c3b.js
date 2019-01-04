5ce072deaa4a89e9591ba3f0c428186928528e28
Alberto Gualis <agualis@gmail.com>
2019-01-04 17:36:36 +0100

    build version

---
+++ docs/precache-manifest.c795cbb32eeac5199f8ec3757f7e3c3b.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = [
+  {
+    "revision": "c38ebd3cd38c98fbd16bf31d1d24ce64",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.c38ebd3c.woff"
+  },
+  {
+    "revision": "f3824a0eb2e75fd4edbef7ae653b559b",
+    "url": "index.html"
+  },
+  {
+    "revision": "a9ff913a3c17b881b663",
+    "url": "js/4b47640d.21cfc388.js"
+  },
+  {
+    "revision": "588b4dec368e3ed4c1d756244afb103b",
+    "url": "statics/icons/icon-512x512.png"
+  },
+  {
+    "revision": "7c0a9017f6f7554c2763",
+    "url": "js/9740257a.6eb58285.js"
+  },
+  {
+    "revision": "6ace500fa7327c17930d4bf3b0c09391",
+    "url": "statics/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "7b725cd4ea6014590192",
+    "url": "js/9c5de6b8.60907f7d.js"
+  },
+  {
+    "revision": "1c0a0f232321e46c2e6bda98fe5d749e",
+    "url": "statics/icons/icon-192x192.png"
+  },
+  {
+    "revision": "ae2f3408e83872f9eede",
+    "url": "js/app.b00a0f89.js"
+  },
+  {
+    "revision": "b9aa277fcfc34c31db6c7a7ea3469b8c",
+    "url": "statics/icons/site.webmanifest"
+  },
+  {
+    "revision": "b668548ebef8a3f1cd27",
+    "url": "js/d115800e.9478a8ee.js"
+  },
+  {
+    "revision": "665003f89a93928162cb",
+    "url": "js/vendor.179da009.js"
+  },
+  {
+    "revision": "c54576b2e283db0fa8192e3272431508",
+    "url": "statics/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "8bbb629a425a659046bb0cbbdba073ce",
+    "url": "statics/icons/favicon.ico"
+  },
+  {
+    "revision": "6be0f7bf6979e44837a903bdc0eec151",
+    "url": "statics/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "d2cb1ba1337c0fb3e01308e3a6a33818",
+    "url": "statics/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "a2508c521ccc7109b86eac441a990dfc",
+    "url": "statics/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a493ba0aa0b8ec8068d786d7248bb92c",
+    "url": "statics/icons/browserconfig.xml"
+  },
+  {
+    "revision": "73badb086f587fc1d786bbd9da73c171",
+    "url": "statics/asalto.gif"
+  },
+  {
+    "revision": "588b4dec368e3ed4c1d756244afb103b",
+    "url": "statics/asalto-logo.png"
+  },
+  {
+    "revision": "e715b1b1a7e487349203d93f7d15316a",
+    "url": "statics/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "2c4487a2ea9f1fdd086e",
+    "url": "js/4102bf14.87d2caa2.js"
+  },
+  {
+    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
+    "url": "fonts/KFOmCnqEu92Fr1Mu4mxM.bafb105b.woff"
+  },
+  {
+    "revision": "659c4d58b00226541ef95c3a76e169c5",
+    "url": "fonts/fa-brands-400.659c4d58.woff2"
+  },
+  {
+    "revision": "8b7a9afd7b95f62e6ee8a72930bfb9ed",
+    "url": "fonts/fa-brands-400.8b7a9afd.woff"
+  },
+  {
+    "revision": "fb493903265cad425ccdf8e04fc2de61",
+    "url": "fonts/fa-solid-900.fb493903.woff2"
+  },
+  {
+    "revision": "bdadb6ce95c5a2e7b673940721450d3c",
+    "url": "fonts/fa-regular-400.bdadb6ce.woff2"
+  },
+  {
+    "revision": "bcb927a742a8370b76642fd1a9a749c0",
+    "url": "fonts/fa-solid-900.bcb927a7.woff"
+  },
+  {
+    "revision": "8a9a261c8b8dfe90db11f1817a9d22e1",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.8a9a261c.woff2"
+  },
+  {
+    "revision": "c2978069c923c7bd6cca2e67554a2b8e",
+    "url": "fonts/urban-decay.c2978069.ttf"
+  },
+  {
+    "revision": "0b5e3a5451fc62d9023ccafc85bc89db",
+    "url": "fonts/fa-regular-400.0b5e3a54.woff"
+  },
+  {
+    "revision": "8c2ade503b34e31430d6c98aa29a52a3",
+    "url": "fonts/KFOlCnqEu92Fr1MmYUtfBBc-.8c2ade50.woff"
+  },
+  {
+    "revision": "de8b7431b74642e830af4d4f4b513ec9",
+    "url": "fonts/KFOlCnqEu92Fr1MmEU9fBBc-.de8b7431.woff"
+  },
+  {
+    "revision": "a1471d1d6431c893582a5f6a250db3f9",
+    "url": "fonts/KFOlCnqEu92Fr1MmSU5fBBc-.a1471d1d.woff"
+  },
+  {
+    "revision": "cf6613d1adf490972c557a8e318e0868",
+    "url": "fonts/KFOlCnqEu92Fr1MmWUlfBBc-.cf6613d1.woff"
+  },
+  {
+    "revision": "4afa30630f63a0a507b1ae17a5b9f88f",
+    "url": "fonts/beatstreet.4afa3063.ttf"
+  },
+  {
+    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
+    "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.e9dbbe8a.woff"
+  },
+  {
+    "revision": "b668548ebef8a3f1cd27",
+    "url": "css/d115800e.591aa189.css"
+  },
+  {
+    "revision": "ae2f3408e83872f9eede",
+    "url": "css/app.d3686877.css"
+  },
+  {
+    "revision": "7b725cd4ea6014590192",
+    "url": "css/9c5de6b8.d0304704.css"
+  },
+  {
+    "revision": "7c0a9017f6f7554c2763",
+    "url": "css/9740257a.641a0eee.css"
+  },
+  {
+    "revision": "2c4487a2ea9f1fdd086e",
+    "url": "css/4102bf14.f243f417.css"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.fc6c7ba686a4717f2b997873ba98184d.js"
+  "precache-manifest.c795cbb32eeac5199f8ec3757f7e3c3b.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "asalto"});
