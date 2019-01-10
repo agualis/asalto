8733940514b73716461c5f5502d47abc62b22fef
Alberto Gualis <agualis@gmail.com>
2019-01-11 00:20:06 +0100

    add docs to gitignore

---
+++ src/layouts/TestModal.vue
@@ -0,0 +1,40 @@
+<template>
+  <q-modal v-model="opened" maximized>
+    <q-toolbar
+      color="primary"
+      :glossy="$q.theme === 'mat'"
+      :inverted="$q.theme === 'ios'"
+    >
+      <ToolbarTitle/>
+    </q-toolbar>
+
+    <h2>TEST</h2>
+    <q-btn @click.native="onClose()"
+           color="primary">
+      CLOSE
+    </q-btn>
+    <List></List>
+  </q-modal>
+</template>
+
+<script>
+  import ToolbarTitle from '../layouts/ToolbarTitle'
+  import List from '../pages/List'
+
+  export default {
+    components: {ToolbarTitle, List},
+    props: {
+      onClose: {type: Function, required: true},
+      opened: {type: Boolean, required: true}
+    },
+    created() {
+      document.addEventListener("backbutton", onBackKeyDown, false)
+      function onBackKeyDown(event) {
+        this.$q.notify({
+        color: 'secondary',
+        message: 'Done opening and closing'
+      })
+      }
+    }
+  }
+</script>
