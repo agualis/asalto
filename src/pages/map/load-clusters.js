f04a8fff38a7a3247b3cc03bca616457ef30ba3b
Alberto Gualis <agualis@gmail.com>
2019-01-22 23:54:19 +0100

    extract functions in loadCluster

--- src/pages/map/load-clusters.js
+++ src/pages/map/load-clusters.js
@@ -2,73 +2,89 @@ const ARTWORKS_SOURCE = 'artworks'
 export const CLUSTER_MAX_ZOOM = 15
 
 export function loadClusters(map, works) {
-  map.addSource(ARTWORKS_SOURCE, {
-    type: "geojson",
-    data: buildClusterData(works),
-    cluster: true,
-    clusterMaxZoom: CLUSTER_MAX_ZOOM, // Max zoom to cluster points on
-    clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
-  })
-  map.addLayer({
-    id: "clusters",
-    type: "circle",
-    source: ARTWORKS_SOURCE,
-    filter: ["has", "point_count"],
-    paint: {
-      // Use step expressions (https://www.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
-      // with three steps to implement three types of circles:
-      //   * Blue, 20px circles when point count is less than 100
-      //   * Yellow, 30px circles when point count is between 100 and 750
-      //   * Pink, 40px circles when point count is greater than or equal to 750
-      "circle-color": [
-        "step",
-        ["get", "point_count"],
-        "#51bbd6",
-        100,
-        "#f1f075",
-        750,
-        "#f28cb1"
-      ],
-      "circle-radius": [
-        "step",
-        ["get", "point_count"],
-        20,
-        100,
-        30,
-        750,
-        40
-      ]
-    }
-  })
 
-  map.addLayer({
-    id: "cluster-count",
-    type: "symbol",
-    source: ARTWORKS_SOURCE,
-    filter: ["has", "point_count"],
-    layout: {
-      "text-field": "{point_count_abbreviated}",
-      "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
-      "text-size": 14
-    }
-  })
+  addGeoJsonArtWorksSource()
+  addLayerForClusterCircles()
+  addLayerForClusterCount()
+  addLayerForUnclusteredPoints()
+  map.on('click', 'clusters', onClusterClick)
 
-   map.addLayer({
-        id: "unclustered-point",
-        type: "circle",
-        source: ARTWORKS_SOURCE,
-        filter: ["!", ["has", "point_count"]],
-        paint: {
-            "circle-color": "#ff2b9e",
-            "circle-radius": 4,
-            "circle-stroke-width": 1,
-            "circle-stroke-color": "#fff"
-        }
+
+  function addGeoJsonArtWorksSource() {
+    map.addSource(ARTWORKS_SOURCE, {
+      type: "geojson",
+      data: buildClusterData(works),
+      cluster: true,
+      clusterMaxZoom: CLUSTER_MAX_ZOOM, // Max zoom to cluster points on
+      clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
     })
+  }
 
-  // inspect a cluster on click
-  map.on('click', 'clusters', function (e) {
-    let works = map.queryRenderedFeatures(e.point, { layers: ['clusters'] })
+  function addLayerForClusterCircles() {
+    map.addLayer({
+      id: "clusters",
+      type: "circle",
+      source: ARTWORKS_SOURCE,
+      filter: ["has", "point_count"],
+      paint: {
+        // Use step expressions (https://www.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
+        // with three steps to implement three types of circles:
+        //   * Blue, 20px circles when point count is less than 100
+        //   * Yellow, 30px circles when point count is between 100 and 750
+        //   * Pink, 40px circles when point count is greater than or equal to 750
+        "circle-color": [
+          "step",
+          ["get", "point_count"],
+          "#51bbd6",
+          100,
+          "#f1f075",
+          750,
+          "#f28cb1"
+        ],
+        "circle-radius": [
+          "step",
+          ["get", "point_count"],
+          20,
+          100,
+          30,
+          750,
+          40
+        ]
+      }
+    })
+  }
+
+  function addLayerForClusterCount() {
+    map.addLayer({
+      id: "cluster-count",
+      type: "symbol",
+      source: ARTWORKS_SOURCE,
+      filter: ["has", "point_count"],
+      layout: {
+        "text-field": "{point_count_abbreviated}",
+        "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+        "text-size": 14
+      }
+    })
+  }
+
+  function addLayerForUnclusteredPoints() {
+    map.addLayer({
+      id: "unclustered-point",
+      type: "circle",
+      source: ARTWORKS_SOURCE,
+      filter: ["!", ["has", "point_count"]],
+      paint: {
+        "circle-color": "#ff2b9e",
+        "circle-radius": 4,
+        "circle-stroke-width": 1,
+        "circle-stroke-color": "#fff"
+      }
+    })
+  }
+
+  function onClusterClick(e) {
+    let works = map.queryRenderedFeatures(e.point, {layers: ['clusters']})
     let clusterId = works[0].properties.cluster_id
     map.getSource(ARTWORKS_SOURCE).getClusterExpansionZoom(clusterId, function (err, zoom) {
       if (err)
@@ -78,8 +94,9 @@ export function loadClusters(map, works) {
         zoom: zoom
       })
     })
-  })
+  }
 }
+
 
 export const buildClusterData = works => (
   {
