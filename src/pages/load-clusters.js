c9ae939ba75156a78eb6010d5319f673be3fe772
Alberto Gualis <agualis@gmail.com>
2019-01-22 08:50:31 +0100

    fix cluster builder

--- src/pages/load-clusters.js
+++ src/pages/load-clusters.js
@@ -85,6 +85,6 @@ export const buildClusterData = works => (
   {
     "type": "FeatureCollection",
     "crs": {"type": "name", "properties": {"name": "urn:ogc:def:crs:OGC:1.3:CRS84"}},
-    "works": works
+    "features": works
   }
 )
